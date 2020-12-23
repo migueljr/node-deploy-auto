6acb5778ff2eaa83d093452d3f20bd98d85a627b
migueljr <miguel_jr@mail.com>
2020-12-23 14:04:59 -0300

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.listen('3000', ()=>{console.log('server init port 3000')});
+
+app.get('/test', (req, res)=>{
+	res.send({'data': 'Sucess'})
+})
+
